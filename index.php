955eb696bdb3538384e1c41b331617f7b6fce060
oumar <57168005+Abib-web@users.noreply.github.com>
2022-06-12 05:36:01 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cv Kone</title>
+</head>
+
+<body>
+    <header>
+        <div class="icon-site">
+            <div class="icon-image"><img src="images/icons8-geek-64.png" alt="icon"></div>
+            <h1> Oumar Kone</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a>Accueil</a></li>
+                <li><a>Competences</a></li>
+                <li><a>Projets</a></li>
+                <li><a>A propos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="slideshow" id="slideshow">
+            <div class="slider-me">
+                <div class="text-slider">
+                    <h1>
+                        Je suis <span style="color: rgb(220, 235, 17);">Oumar Kone.</span>
+                    </h1>
+                    <br>
+                <br>
+                <br>
+                <br>
+                    <ul>
+                        <li>
+                            Informaticien à <span> <a href="#" style="color: #7A198E;text-decoration:none"> Xcience </a></span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="fill">
+                        <img src="images/me.jpeg" alt="">
+                </div>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <div class="liens">
+                        <a href="" style="text-decoration: none;"> Mes Competences</a>
+                </div>
+            </div>
+            <div class="slider-hadware">
+                <div class="text-slider">
+                    <h1>
+                        Je suis Etudiant en<span style="color: rgb(220, 235, 17);"> genie electrique
+                        concentration genie informatique </span>
+                    </h1>
+                    <br>
+                    <br>
+                    <br>
+                    <ul>
+                        <li>
+                            Programmation de VHDL
+                        </li>
+                        <li>
+                            Programmation de Pic
+                        </li>
+                        <li>
+                            Programmation de micro-contrôleur
+                        </li>
+                    </ul>
+                </div>
+                <div class="fill">
+                        <img src="images/connect.jpeg" alt="">
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="liens">
+                        <a href="" style="text-decoration: none;"> Mes Projets</a>
+                </div>
+            </div>
+            <div class="slider-fullstack">
+                <div class="text-slider">
+                    <h1>
+                        Je suis <span style="color: rgb(220, 235, 17);">developpeur Full-Stack.</span>
+                    </h1>
+                    <br>
+                    <br>
+                    <br>
+                    <ul>
+                        <li>Backend</li>
+                        <li>Frontend</li>
+                    </ul>
+                </div>
+                <div class="fill">
+                        <img src="images/fullstack.jpeg" alt="">
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="liens">
+                        <a href="" style="text-decoration: none;"> Mes Competences</a>
+                </div>
+            </div>
+            <div class="slider-embed">
+                <div class="text-slider">
+                    <h1>
+                        Je suis <span style="color: rgb(220, 235, 17);">developpeur systeme embarqué. </span>
+                    </h1>
+                    <br>
+                    <br>
+                    <br>
+                    <ul>
+                        <li>Stm32</li>
+                        <li>Carte arduino</li>
+                        <li>Omega Onion</li>
+                        <li>Raspberry</li>
+                        <li>Pic</li>
+                    </ul>
+                </div>
+                <div class="fill">
+                        <img src="images/ci.jpeg" alt="">
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="liens">
+                        <a href="" style="text-decoration: none;">  Projets en systeme embarqué</a>
+                </div>
+            </div>
+        </div>
+        <br>
+
+        <div class="flex-container about-me">
+            <div class="img-about-me">
+                <img src="images/info.png" alt="">
+            </div>
+            <div class="img-presentation">
+                <h1 style="color: rgba(255, 255, 255, 0.1);
+    z-index: 10;
+    font-size: 50px;"> Me concernant</h1>
+                <p>
+                <ul>
+                    <li>
+                        <span>Nom : </span>  <span> Kone</span>
+                    </li>
+                    <li>
+                        <span>Prénoms :</span> <span>Oumar</span>
+                    </li>
+                    <li>
+                        <span>Langues :</span> <span> Francais(professionnel) & Anglais(Debutant)</span>
+                    </li>
+                    <li>
+                        <span>Ville: </span> <span>Montréal </span>
+                    </li>
+                    <li>
+                        <span></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    <div class="career">
+        <h1 class="title-parcours">Mon Parcours scolaire</h1>
+        <div class="career-1">
+            <h1 class="career-1-title">Actuellement</h1>
+            <p class="career-1-paragraph">
+                Etudiant en baccalauréat en genie electrique concetration genie informatique 3e année à UQTR
+            </p>
+                <img src="images/uqtr.png" alt="logo-uqtr" style="height: 50px; width: 150px; margin-top: 20px">
+                <img src="images/gegi.jpg" alt="logo-gegi" style="height: 50px; width: 150px; margin-top: 20px">
+        </div>
+        <div class="career-2">
+        <h1 class="career-2-title">2018-2019</h1>
+            <p class="career-2-paragraph">
+                Etudiant en BTS en Informatique developpeur d'application 1e année à l'esetec
+            </p>
+                <img src="images/esetec.jpg" alt="logo-esetec" style="height: 50px; width: 250px; margin-top: 20px; display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;">
+        </div>
+        <div class="career-3">
+        <h1 class="career-3-title">2015-2018</h1>
+            <p class="career-3-paragraph">
+                Elève en baccalauréat scientifique(serie D) au collège Guchanorlain
+            </p>
+                <img src="images/guchanrolain.jpg" alt="logo-guchanrolain" style="height: 50px; width: 50px; margin-top: 20px; display: block;
+  margin-left: auto;
+  margin-right: auto;
+  ">
+        </div>
+        <h1 class="title-parcours-2">Mon Parcours scolaire</h1>
+    </div>
+    <div class="competences">
+        <h1>Competences</h1>
+        <div class="progress-bar">
+        <p>Langage c</p>
+        <progress  max="100" value="70" ></progress>
+        <p>Python</p>
+        <progress max="100" value="70"></progress>
+        </div>
+    </div>
+    </main>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="script/script.js"></script>
+</body>
+
+</html>
---
+++ script/script.js
@@ -0,0 +1,57 @@
+/*document.querySelectorAll( '.ciclegraph' ).forEach( ( ciclegraph )=>{
+    let circles = ciclegraph.querySelectorAll( '.circle' )
+    let angle = 360-90, dangle = 360 / circles.length
+    for( let i = 0; i < circles.length; ++i ){
+      let circle = circles[i]
+      angle += dangle
+      circle.style.transform = `rotate(${angle}deg) translate(${ciclegraph.clientWidth / 2}px) rotate(-${angle}deg)`
+    }
+  })
+var sliderListe = ["slider-me","slider-hadware","slider-fullstack","slider-embed"];
+
+function slideFunction(){
+  for(let i = 0; i < sliderListe.length; ++i ){
+    for(let x = 0; x < sliderListe.length; ++x ){
+      if(x === i){
+        continue
+      }else{
+        var elems_hide = document.getElementsByClassName(sliderListe[x]);
+        hideContent(elems_hide);
+      }
+    }
+    setInterval(function(){
+      var elems = document.getElementsByClassName(sliderListe[i]);
+      displayContent(elems);
+    }, 1000)
+
+  }
+  
+  //startSlide()
+}
+function startSlide(){
+  var elems = document.getElementsByClassName('slider-me');
+  displayContent(elems);
+}
+  
+  function displayContent(div){
+    for (var i=0;i<div.length;i+=1){
+      div[i].style.display = 'block';
+    }
+  }
+  function hideContent(div){
+    for (var i=0;i<div.length;i+=1){
+      div[i].style.display = 'none';
+    }
+  }
+  window.onload = slideFunction*/
+
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(500)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
